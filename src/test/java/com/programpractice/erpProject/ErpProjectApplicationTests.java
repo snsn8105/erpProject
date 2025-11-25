3565c44a3880206c2eed65208198f26b2f3a0a3f
snsn8105 <snsn8105@naver.com>
2025-11-25 15:38:05 +0900

    feat: spring boot 초기화

---
+++ src/test/java/com/programpractice/erpProject/ErpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programpractice.erpProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
